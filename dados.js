ce42387b6638ab576c31e9aa1cd6d853e73c79c2
marcos0505 <jmsp.marcos@gmail.com>
2022-02-08 21:32:56 -0300

    Criaçao de chat

--- dados.js
+++ dados.js
@@ -1,6 +1,14 @@
 
-const a = 5
-const b = 10
-const soma =  a + b
-console.log (soma);
+//const a = 5
+//const b = 10
+//console.log (a + b);
+
+
+//var programação = 'facio'
+//programação = 'dificil'
+//console.log('programação');
+ let a = 2
+ let b = 5
+ console.log(a + b);
+ 
 
