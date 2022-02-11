3c0670af8af0179c25567d9e65c37261af744faf
marcos0505 <jmsp.marcos@gmail.com>
2022-02-10 21:15:18 -0300

    Apresentacao das condicoes

--- Chat.js
+++ Chat.js
@@ -13,25 +13,19 @@
 // };
 
 const input = require("readline-sync");
- console.log("Seja vindo! Vamos correr o mais rapido que poder! ")
+console.log("Seja vindo! corra os 10km mais rapido que poder! ");
 
- const corrida = input.question("Em quanto tempo voce correu os 10Km? ");
-
- if (corrida <=30) {
-     console.log('Parabens!!!, Voce vai para as olimpiadas')
- } else if(corrida >30 && corrida <=32) {
-     console.log("Voce está na repescagem! Boa sorte!")
- } else {
-     console.log("Não foi desta vez! continue treinando, e boa sorte na proxima.")
- };
-
-
-
-
-
-
-
-
-
-
- 
+const corrida = input.question("Em quanto tempo voce correu os 10Km? ");
+if (corrida > "1") {
+  if (corrida <= "30") {
+    console.log("Parabens!!!, Voce vai para as olimpiadas");
+  } else if (corrida > "30" && corrida <= "32") {
+    console.log("Voce está na repescagem! Boa sorte!");
+  } else {
+    console.log(
+      "Não foi desta vez! continue treinando, e boa sorte na proxima."
+    );
+  }
+}else{
+    console.log("Tempo inválido"); 
+}
