d1f5eb65099081c78fb042e5c5809312e6b7e34c
marcos0505 <jmsp.marcos@gmail.com>
2022-02-15 21:08:45 -0300

    aplicacao de array

---
+++ array.js
@@ -0,0 +1,22 @@
+// const lista = new Array('pera', 'uva', 'maca')
+
+// const tamanho = (lista)
+
+// console.log(tamanho.length)
+
+
+
+const cadastro = [
+{Nome:'Marcos', Curso: 'Desenvolvimento Web beck End',turno: 'noite', Matricula: '2022'},
+{Nome:'Deyvson', Curso: 'Desenvolvimento Web beck End',turno: 'noite', Matricula: '2020'},
+{Nome:'Matheus', Curso: 'Desenvolvimento Web beck End',turno: 'noite', Matricula: '2021'},
+{Nome:'Edson', Curso: 'Desenvolvimento Web beck End',turno: 'noite', Matricula: '2023'},
+{Nome:'Riva', Curso: 'Desenvolvimento Web beck End',turno: 'noite', Matricula: '2024'}
+]
+
+const primeiroItem = cadastro [1]//inicio de contagem da array
+const dados = (cadastro) //mostra todos os itens da array
+
+console.log(dados.length)// quantidade de itens da array
+console.log(dados)//descrição dos 
+console.log('Matricula: ' ,primeiroItem.Matricula) //console para filtro, escolher o item.
